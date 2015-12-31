6dbbef1cc77f4377817d8988f86fea29ebafdfe2
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-12-31 11:09:10 -0500

    Add all channel components

---
+++ README.md
@@ -0,0 +1,6 @@
+# Learn React
+This repo is being used to learn React. I am following along with the videos posted here:
+http://courses.knowthen.com/courses/learn-how-to-develop-realtime-web-apps
+
+## Setup
+From the directory `/rtsupport` run `npm install`.
---
+++ components/channels/ChannelList.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import Channel from './Channel.jsx';
+
+class ChannelList extends Component{
+  render(){
+    return(
+      <ul>
+        this.props.channels(chan =>{
+          <Channel
+            channel = {chan}
+            setChannel = {this.props.setChannel}
+          />
+        })
+      </ul>
+    )
+  }
+}
+
+ChannelList.propTypes = {
+  channels: React.PropTypes.array.isRequired,
+  setChannel: React.PropTypes.func.isRequired
+}
